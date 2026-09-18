a0e2f50097480899905947118a727bc5e28596d0
Maria Jose Uribe <105825647+mjuribe@users.noreply.github.com>
2026-09-18 18:29:16 -0500

    Taller pre-03, inicio pre-04

---
+++ PRE_04_limpieza/src/diagnostics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
+
+
+def main():
+    df = pd.read_csv(OUTPUT_FILE)
+    #series = df["country"]
+    series = df["purchase_date"]
+
+    series = series[series.str.contains(r"-\d{2}$", regex=False)]
+
+    series = series.sort_values()
+    series = series.drop_duplicates()
+
+    print(series)
+    print(len(series))
+
+
+if __name__ == "__main__":
+    main()
